b9b0bddeb1726d535a1576c16f62ff361bb81804
jthanawat <kla13thanawat@gmail.com>
2020-07-31 15:43:02 +0700

    update page

--- frontend/src/pages/Questionnaire/components/FinishedPage.js
+++ frontend/src/pages/Questionnaire/components/FinishedPage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { makeStyles, Icon, Grid, IconButton, Typography } from '@material-ui/core'
 import {
   Card,
   CardContent
@@ -6,17 +7,68 @@ import {
 import CheckCircleSharpIcon from '@material-ui/icons/CheckCircleSharp';
 import TopbarForm from '../../../layout/Form/Topbar'
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: '100%',
+    padding: theme.spacing(10)
+  },
+  card: {
+    textAlign: 'center',
+    width: '400px',
+  },
+  icon: {
+    color: theme.palette.primary.main,
+  }
+}))
+
 const FinishedPage = () => {
+  const classes = useStyles();
+
   return (
-    <>
-    <TopbarForm />
-    <Card>
-      <CardContent>
-        Thank you for taking the time
-        to complete our survey!
-      </CardContent>
-    </Card>
-    </>
+    <div
+      className={classes.root}>
+      <TopbarForm />
+      <Grid
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+      >
+        <Grid
+          item xs={12} md={12} lg={12}
+        >
+
+          <Card
+            className={classes.card}
+          >
+            <CardContent
+            >
+              <Typography
+                variant="h5"
+              >
+                Thank you for taking your time
+              </Typography>
+            </CardContent>
+            <CardContent>
+              <Typography
+                variant="h5"
+              >
+                to complete our survey!
+              </Typography>
+            </CardContent>
+            <IconButton
+              className={classes.icon}
+
+            >
+              <CheckCircleSharpIcon
+                fontSize='large'
+              />
+            </IconButton>
+          </Card>
+
+        </Grid>
+      </Grid>
+    </div>
   )
 }
 
